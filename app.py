ac583a798d409e368eee488968546840c3fbc570
AgnesNM <agnesndutamwaniki@gmail.com>
2024-09-30 11:12:21 +0300

    data frame

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ import pandas as pd
 
 # import numpy as np
 
-df = pd.DataFrame({
-    'first column': [1,2,3,4],
-    'second column':[ 10, 20, 30, 40]
-})
+# df = pd.DataFrame({
+#     'first column': [1,2,3,4],
+#     'second column':[ 10, 20, 30, 40]
+# })
 
-df
+# df
 
-# st.write("Here's our first attempt at using data to create a table")
+st.write("Here's our first attempt at using data to create a table")
 
-# st.write(pd.DataFrame({
-#     'first column': [ 1,2,3,4 ],
-#     'second column': [10,20,30,40]
-# }))
+st.write(pd.DataFrame({
+    'first column': [ 1,2,3,4 ],
+    'second column': [10,20,30,40]
+}))
 
 # dataframe = np.random.randn(10,20)
 
