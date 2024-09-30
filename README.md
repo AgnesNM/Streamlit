31ef6c7e61c1e5360a8f78e61b2273aa91db5766
AgnesNM <agnesndutamwaniki@gmail.com>
2024-09-30 11:18:00 +0300

    static table generation and README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Basic concepts of Streamlit
+
+streamlit run app.py
+
+Local URL: http://localhost:8501
--- app.py
+++ app.py
@@ -26,9 +26,16 @@ import numpy as np
 
 # st.dataframe(dataframe)
 
+# dataframe = pd.DataFrame(
+#     np.random.randn(10,20),
+#     columns = ('col %d' % i for i in range(20))
+# )
+
+# st.dataframe(dataframe.style.highlight_max(axis=0))
+
 dataframe = pd.DataFrame(
     np.random.randn(10,20),
     columns = ('col %d' % i for i in range(20))
 )
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
+st.table(dataframe)
